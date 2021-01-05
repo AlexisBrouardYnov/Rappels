6ca355e4f97405ba225fac41bb93a91c3b6085aa
AlexisBrouardYnov <67193389+AlexisBrouardYnov@users.noreply.github.com>
2021-01-05 13:41:00 +0100

    05/01

--- app/src/main/java/com/ynov/brouard/projetrappels/MainActivity.java
+++ app/src/main/java/com/ynov/brouard/projetrappels/MainActivity.java
@@ -3,20 +3,31 @@ package com.ynov.brouard.projetrappels;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.core.Tag;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +35,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FloatingActionButton ajouterRappel = findViewById(R.id.ajouterRappel);
+        mDatabase = FirebaseDatabase.getInstance().getReference("rappels");
+
+        lireRappel();
 
         ajouterRappel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent gameActivity = new Intent(MainActivity.this, creerRappel.class);
-                startActivity(gameActivity);
+                Intent IntentActivity = new Intent(MainActivity.this, creerRappel.class);
+                startActivity(IntentActivity);
             }
         });
     }
@@ -49,5 +63,42 @@ public class MainActivity extends AppCompatActivity {
                 dialog.dismiss();
             }
         }).show();
+    }
+
+    private void lireRappel() {
+        ArrayList<Map<String, Object>> mRappel = new ArrayList<Map<String, Object>>();
+        ListView listeRappel = findViewById(R.id.listeRappel);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2);
+        listeRappel.setAdapter(arrayAdapter);
+
+
+        mDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Map<String, Object> map = (Map<String, Object>) snapshot.getValue();
+                mRappel.add(map);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
     }
 }
--- app/src/main/java/com/ynov/brouard/projetrappels/creerRappel.java
+++ app/src/main/java/com/ynov/brouard/projetrappels/creerRappel.java
@@ -1,5 +1,6 @@
 package com.ynov.brouard.projetrappels;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,26 +9,37 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class creerRappel extends AppCompatActivity {
 
-    private DatabaseReference mDatabase;
+    DatabaseReference mDatabase;
     String Stitre;
     String Scontenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_creer_rappel);
 
         EditText Etitre = findViewById(R.id.editTitle);
         EditText Econtenu = findViewById(R.id.editContent);
         FloatingActionButton Bvalidation = findViewById(R.id.validerButton);
+        TextView Tvalide = findViewById(R.id.txtEnvoye);
+        TextView TverifChamps = findViewById(R.id.verifChamps);
 
+        Tvalide.setVisibility(View.INVISIBLE);
+        TverifChamps.setVisibility(View.INVISIBLE);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
@@ -36,16 +48,22 @@ public class creerRappel extends AppCompatActivity {
             public void onClick(View v) {
                 Stitre = Etitre.getText().toString();
                 Scontenu = Econtenu.getText().toString();
-                ecrireRappel(Stitre,Scontenu);
-                Bvalidation.setVisibility(View.INVISIBLE);
+
+                if (Stitre.equals("") || Scontenu.equals("")) {
+                    TverifChamps.setVisibility(View.VISIBLE);
+                }
+                else {
+                    ecrireRappel(Stitre,Scontenu);
+                    TverifChamps.setVisibility(View.INVISIBLE);
+                    Bvalidation.setVisibility(View.INVISIBLE);
+                    Tvalide.setVisibility(View.VISIBLE);
+                }
             }
         });
     }
     private void ecrireRappel(String titre, String contenu) {
-
         Rappel rappel = new Rappel(titre,contenu);
         //ECRIRE
-
         mDatabase.child("rappels").child(titre).setValue(rappel);
     }
     /*private void modifierRappel(String Id) {
@@ -53,3 +71,4 @@ public class creerRappel extends AppCompatActivity {
     }*/
 
 }
+
